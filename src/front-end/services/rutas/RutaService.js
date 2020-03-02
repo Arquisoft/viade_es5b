1fc474d81854d4f28e2128126d0b6285be70015a
Alex Florez <UO264371@uniovi.es>
2020-03-02 19:13:46 +0100

    Añadiendo los servicios iniciales de las rutas

---
+++ src/front-end/services/rutas/RutaService.js
@@ -0,0 +1,38 @@
+"use strict";
+/*
+ * Clase que representa el servicio de Rutas (Fachada)
+ */
+class RutaService {
+  constructor() {
+    // Simula la base de datos
+    this.BDRutas = [];
+  }
+
+  /*
+   * Método de prueba que inicializa la lista
+   * que simula la base de datos.
+   */
+  inicializarBD() {
+    var r1 = new Ruta("Ruta-1", "Avilés");
+    var r2 = new Ruta("Ruta-2", "Oviedo");
+
+    r1.addHito(new Hito("Hito-r1-1", 10, 50));
+    r1.addHito(new Hito("Hito-r1-2", 50, 10));
+
+    r2.addHito(new Hito("Hito-r2-1", 10.4, 0));
+    r2.addHito(new Hito("Hito-r2-2", 520, -10));
+  }
+
+  /*
+   * Devuelve TODAS las rutas almacenadas en
+   * el sistema.
+   */
+  getRutas() {
+    return this.BDRutas;
+  }
+
+  /*
+   * Añade una nueva ruta al sistema.
+   */
+  addRuta(ruta) {}
+}
