1206d4a784ca59e84a8dbf3343360c7b6997e49b
UO247346 <47303435+UO247346@users.noreply.github.com>
2020-03-05 10:45:35 +0100

    Nuevos metodos en el main

--- src/BackEnd/BackMain.js
+++ src/BackEnd/BackMain.js
@@ -1,10 +1,21 @@
 import React from "react";
 import {useWebId} from "@inrupt/solid-react-components";
 import {initialiseRoutesList} from './Scripts/storageForRoutes.js'
+import{getRoutesList} from './Scripts/getNotesStorage.js'
+var Usuario = new Object();
+Usuario.iD=inicio();
+Usuario.rutasStorage=creacionAlmacenRutas();
 inicio = () =>{
-    const WebId = useWebId();
+    let WebId = useWebId();
+    return WebId;
 };
 
 creacionAlmacenRutas = () =>{
-    let rutasStorage= initialiseRoutesList(profile, typeIndex);
+    let rutasStorageNew= initialiseRoutesList(profile, typeIndex);
+    return rutasStorageNew;
+};
+
+obtenerAlmacenRutas = () =>{
+    let rutasStorage= getRoutesList(profile);
+    return rutasStorage
 };
