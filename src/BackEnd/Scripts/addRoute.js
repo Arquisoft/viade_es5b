e774e8f24e432d153bcaff4d1a18b06c59309068
pedro jose fernandez franco <uo224775@uniovi.es>
2020-03-05 13:07:04 +0100

    Añadido gitignore tokens servidor  y obtenida sesion usuario en addRoute

--- src/BackEnd/Scripts/addRoute.js
+++ src/BackEnd/Scripts/addRoute.js
@@ -2,7 +2,13 @@ import{ruta} from '../Models/Ruta.js'
 const auth = require('solid-auth-client')
 
 export async function addRoute() {
-    auth.fetch('https://timbl.com/timbl/Public/friends.ttl')
-  .then(console.log);
+    auth.trackSession(session => {
+        //si no esta logueado lo redirijo al login.
+        if (!session)
+            window.location.href = "/login";
+        //si esta logueado guardo la ruta
+        else
+          console.log(`The user is ${session.webId}`)
+      })
 }
 
