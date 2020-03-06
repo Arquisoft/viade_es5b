f4d911088b5b371ceecae49c7a3392e920017a8a
Alex Florez <UO264371@uniovi.es>
2020-03-06 16:10:48 +0100

    Comenzando a incluir el mapa con leaflet

---
+++ src/front-end/components/map/Map.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+    constructor(){
+        this.state = {};
+    }
+
+
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Map;
