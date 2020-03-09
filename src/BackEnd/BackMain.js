e4452bc3cf42086409de406ca66d345ccb07b5a9
UO247346 <UO247346@users.noreply.github.com>
2020-03-09 16:14:58 +0100

    Ahora, como en el front end, tenemos el perfil del usuario loggeado

--- src/BackEnd/BackMain.js
+++ src/BackEnd/BackMain.js
@@ -5,15 +5,21 @@ import {useWebId} from "@inrupt/solid-react-components";
 import {initialiseRoutesList} from './Scripts/storageForRoutes.js'
 import{getRoutesList} from './Scripts/getNotesStorage.js'
 import{addRoute} from './Scripts/addRoute.js'
-
+const auth = require('solid-auth-client')
 var Usuario = new Object();
 Usuario.iD=BackMain.inicio();
 Usuario.rutasStorage=BackMain.creacionAlmacenRutas();
 export default class BackMain
 {
     static Inicio(){
-        let WebId = useWebId();
-        return WebId;
+    auth.trackSession(session => {
+        //si no esta logueado lo redirijo al login.
+     if (!session)
+        window.location.href = "/login";
+            //si esta logueado guardo la ruta
+    else
+        console.log(`The user is ${session.webId}`)
+       })
     };
 
     static creacionAlmacenRutas()
