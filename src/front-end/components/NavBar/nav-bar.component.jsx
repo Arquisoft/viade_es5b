fa80af9ab32ea332c379cc95324a1999a7333a9e
Alex Florez <UO264371@uniovi.es>
2020-03-18 20:04:23 +0100

    intento de arreglo de deploy

--- src/front-end/components/NavBar/nav-bar.component.jsx
+++ src/front-end/components/NavBar/nav-bar.component.jsx
@@ -3,14 +3,13 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
 const NavBar = () => {
-
   return (
     <Navbar bg="primary" variant="dark">
-        <Navbar.Brand href="/viade_es5b/">Viade-Rutas</Navbar.Brand>
-        <Nav className="ml-auto">
-          <Nav.Link href="/viade_es5b/login">Log In</Nav.Link>
-          <Nav.Link href="/viade_es5b/signup">Sign Up</Nav.Link>
-        </Nav>
+      <Navbar.Brand href="/">Viade-Rutas</Navbar.Brand>
+      <Nav className="ml-auto">
+        <Nav.Link href="/login">Log In</Nav.Link>
+        <Nav.Link href="/signup">Sign Up</Nav.Link>
+      </Nav>
     </Navbar>
   );
 };
