12ce89a2d40d305e94ec09e119d81cd7ab9b3451
Alejandro Iglesias Calvo <uo247346@uniovi.es>
2020-03-25 17:07:51 +0100

    añadido el import

--- src/BackEnd/BackMain.js
+++ src/BackEnd/BackMain.js
@@ -1,7 +1,7 @@
 import { addRoute } from './Scripts/addRoute.js'
 import { deleteRoute } from './Scripts/deleteRoute.js'
 import { listRoutes } from './Scripts/listRoutes.js'
-
+import {addFriend} from './Scripts/addFriend.js'
 export default class BackMain {
     static listarRutas() {
         return listRoutes()
