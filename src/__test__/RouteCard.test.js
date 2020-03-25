8dfc82eab10fd0a4a2f9ab17b8f8b3bdce2c48b3
Alex Florez <UO264371@uniovi.es>
2020-03-25 20:35:44 +0100

    Desarrollo TDD de comprobación de los hitos de una ruta

--- src/__test__/RouteCard.test.js
+++ src/__test__/RouteCard.test.js
@@ -8,6 +8,11 @@ import "@testing-library/jest-dom";
 
 // Ruta de prueba
 let ruta = new Ruta("Ruta Avilés", [4.5, 5.4], "Muy buena ruta, entretenida.");
+let hito1 = new Hito("Niemeyer", 4, 3.2);
+let hito2 = new Hito("Parque Ferrera", 4.5, 5.7);
+ruta.addHito(hito1);
+ruta.addHito(hito2);
+console.log("-------------------" + ruta.getHitos().length);
 
 test("se renderiza sin fallos", () => {
   const div = document.createElement("div");
@@ -20,4 +25,11 @@ test("RouteCard contiene la información básica de la ruta.", () => {
   expect(getByTestId("r-description")).toHaveTextContent(ruta.getDescripcion());
 });
 
-test("RouteCard contiene la información del inicio y los hitos de la ruta.", () => {});
+test("RouteCard contiene la información del inicio y los hitos de la ruta.", () => {
+  const { getByTestId } = render(<RouteCard ruta={ruta}></RouteCard>);
+  const body = getByTestId("r-hitos");
+  const rows = body.children;
+  expect(rows[0].children[0].textContent).toBe("Inicio");
+
+  //expect(body.children.length).toBe(3);// TODO: esperar a que arregle lucia AñadirRuta
+});
