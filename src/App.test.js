834a30bfea04b05498b6b932867dbc4d173a3589
DiegoMarcosPerez <47299591+DiegoMarcosPerez@users.noreply.github.com>
2020-03-25 12:00:17 +0100

    Probando tests en la clase app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Viade - Arquitectura del Software/i);
-  expect(linkElement).toBeInTheDocument();
+it('Prueba de que la aplicación ejecuta', () =>{
+  afterAll(cleanup);
+  const {container} = render(<App />);
+  expect(container).toBeTruthy(); 
 });
