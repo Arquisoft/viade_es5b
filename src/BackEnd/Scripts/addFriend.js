b7fb3cdf28c7ac1fe97ff34f0f3ed84e95c08c60
Alex Florez <UO264371@uniovi.es>
2020-03-26 17:51:10 +0100

    Arreglando error Travis: warning de concatenación de strings redundante

--- src/BackEnd/Scripts/addFriend.js
+++ src/BackEnd/Scripts/addFriend.js
@@ -1,62 +1,58 @@
-import { rdf } from 'rdf-namespaces';
+import { rdf } from "rdf-namespaces";
 import { fetchDocument } from "tripledoc";
-const auth = require('solid-auth-client')
-
+const auth = require("solid-auth-client");
 
 //Añade el usuario a amigos, si logra añadirlo devuelve true sino false.
-export async function addFriend(friendWebId,friendAlias) {
-    let session = await auth.currentSession();
-    if (!session) { window.location.href = "/login"; }
+export async function addFriend(friendWebId, friendAlias) {
+  let session = await auth.currentSession();
+  if (!session) {
+    window.location.href = "/login";
+  }
 
-    const webId = session.webId;
+  const webId = session.webId;
 
-
-    //Compruebo si ya es mi amigo, si no lo es lo añado
-    var friend=false;
-    var result=false;
-    const profileDoc = await fetchDocument(webId);
-    const profile = profileDoc.getSubject(webId);
-    var friends = profile.getAllNodeRefs('http://xmlns.com/foaf/0.1/knows');
-    for (var i = 0; i < friends.length; i+=1)
-     {
-        if(friends[i]=== friendWebId)
-        {
-            console.log(friends[i]+" vs "+friendWebId);
-            friend= true;
-        }
+  //Compruebo si ya es mi amigo, si no lo es lo añado
+  var friend = false;
+  var result = false;
+  const profileDoc = await fetchDocument(webId);
+  const profile = profileDoc.getSubject(webId);
+  var friends = profile.getAllNodeRefs("http://xmlns.com/foaf/0.1/knows");
+  for (var i = 0; i < friends.length; i += 1) {
+    if (friends[i] === friendWebId) {
+      console.log(friends[i] + " vs " + friendWebId);
+      friend = true;
     }
-    //Si no es amigo lo añado
-    if(!friend)
-    {
-        console.log(friendWebId+" "+friendAlias+" "+" no existe");
-        await insertData(webId,friendWebId,friendAlias);
-        result=true;
-    }
-    return result;
+  }
+  //Si no es amigo lo añado
+  if (!friend) {
+    console.log(friendWebId + " " + friendAlias + "  no existe");
+    await insertData(webId, friendWebId, friendAlias);
+    result = true;
+  }
+  return result;
 }
 
 //https://unhosted.org/using-solid/
 //https://codesandbox.io/s/peaceful-payne-su5t6?fontsize=14
-async function insertData(webId,friendWebId, alias) {
-    const profileDocument = await fetchDocument(webId);
+async function insertData(webId, friendWebId, alias) {
+  const profileDocument = await fetchDocument(webId);
 
-    // Inicializamos un Subject Persona:
-    const newFriend = profileDocument.addSubject({
-        identifier: friendWebId
-    });
-    // Indicamos que el Subject es Una Persona
-    newFriend.addRef(rdf.type, 'http://xmlns.com/foaf/0.1/person');
+  // Inicializamos un Subject Persona:
+  const newFriend = profileDocument.addSubject({
+    identifier: friendWebId
+  });
+  // Indicamos que el Subject es Una Persona
+  newFriend.addRef(rdf.type, "http://xmlns.com/foaf/0.1/person");
 
-    // Le añadimos con su alias
-    newFriend.addString('http://xmlns.com/foaf/0.1/label', alias);
+  // Le añadimos con su alias
+  newFriend.addString("http://xmlns.com/foaf/0.1/label", alias);
 
-    //Añadimos un nuevo Subject de que Conocemos a la Persona (es amigo)
-    const newKnown = profileDocument.addSubject({
-        identifier: 'me'
-    });
+  //Añadimos un nuevo Subject de que Conocemos a la Persona (es amigo)
+  const newKnown = profileDocument.addSubject({
+    identifier: "me"
+  });
 
-    newKnown.addRef('http://xmlns.com/foaf/0.1/knows',friendWebId)
+  newKnown.addRef("http://xmlns.com/foaf/0.1/knows", friendWebId);
 
-
-    await profileDocument.save([newFriend,newKnown]);
+  await profileDocument.save([newFriend, newKnown]);
 }
