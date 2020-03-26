39baea1627a3a178002e7611e9bf6b9711785334
adnanmouloud <adnanmouloud@gmail.com>
2020-03-26 11:48:58 +0100

    test [se muestra el botón de eliminar]

--- src/front-end/components/ruta/VerRutas/RouteCard.jsx
+++ src/front-end/components/ruta/VerRutas/RouteCard.jsx
@@ -31,6 +31,7 @@ class RouteCard extends Component {
             variant="danger"
             className="mr-2"
             onClick={() => this.props.handleDelete(this.props.ruta.getUUID())}
+            data-testid="r-deleteButton"
           >
             Eliminar
           </Button>
