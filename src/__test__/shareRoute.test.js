86ef5a50f0d03620a429ed39602c557447cc93d1
adnanmouloud <adnanmouloud@gmail.com>
2020-03-29 13:37:52 +0200

    empezando test compartir

---
+++ src/__test__/shareRoute.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import RouteCard from "../front-end/components/ruta/VerRutas/RouteCard";
+import Ruta from "../front-end/model/Ruta";
+import Hito from "../front-end/model/Hito";
+import { render, cleanup, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+
+
+// Rutas de prueba
+let ruta = new Ruta("Ruta Avilés", [4.5, 5.4], "Muy buena ruta, entretenida.");
+let hito1 = new Hito("Niemeyer", 4, 3.2);
+let hito2 = new Hito("Parque Ferrera", 4.5, 5.7);
+ruta.addHito(hito1);
+ruta.addHito(hito2);
+
+console.log("-------------------" + ruta.getHitos().length);
+
+
+test("RouteCard contiene el botón de compartir.", () => {
+    const { getByTestId } = render(<RouteCard ruta={ruta}></RouteCard>);
+    expect(getByTestId("rb-compartir")).toHaveTextContent("Compartir");
+});
