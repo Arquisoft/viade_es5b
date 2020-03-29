98e03eb5b2bc246ef8706c86af32e0db0855a20c
Pedro José Fernández Franco <uo224775@uniovi,es>
2020-03-29 20:09:53 +0200

    Ya manda notificacion a los interesados, pero aun no la proceso

--- src/BackEnd/Scripts/addCommentToMyRoute.js
+++ src/BackEnd/Scripts/addCommentToMyRoute.js
@@ -1,7 +1,7 @@
 import { rdf, schema } from 'rdf-namespaces';
 import { fetchDocument } from 'tripledoc';
 import {findRouteURL} from "./helpers/routeHelper";
-import {getRootStorage} from "./helpers/fileHelper";
+import {getRootStorage,existsFile} from "./helpers/fileHelper";
 import {sendNotificationBody} from "./helpers/notificationHelper";
 
 
@@ -19,15 +19,21 @@ export async function addCommentToMyRoute(comentario,routeUUID){
     //Si no la encuentro la busco en publico
     if (url===null) 
         url= await findRouteURL(storage + 'public/routes/',routeUUID);
-    //Si la encuentro entonces inserto el comentario
+    //Si la encuentro entonces inserto el comentario y mando una circular
     if (url!==null) 
-        insertData(comentario,url,routeUUID,webId)
+    {
+        insertData(comentario,url,webId);
+        //Busco a que amigos mandar la circular y las mando
+        var friends = await getSharedRouteFriends(storage,routeUUID);
+        for(let i=0;i<friends.length;i++)
+        {
+            sendCommentNotification(webId,friends[i],routeUUID);
+        }
+    }
 }
 
-async function insertData(comentario, routeUrl,routeUUID,myWebId) {
-    const routeDocument = await fetchDocument(routeUrl);
-    let ruta = routeDocument.getSubject('http://arquisoft.github.io/viadeSpec/route');
-    
+async function insertData(comentario, routeUrl,myWebId) {
+    const routeDocument = await fetchDocument(routeUrl);    
     // Initialise the new Subject:
     const newComment = routeDocument.addSubject();
 
@@ -37,9 +43,33 @@ async function insertData(comentario, routeUrl,routeUUID,myWebId) {
     newComment.addRef(rdf.type, 'http://arquisoft.github.io/viadeSpec/userComment');
 
     await routeDocument.save([newComment]);
-
-    //sendCommentNotification(myWebId,,routeUUID);
 }
+async function getSharedRouteFriends(storage,routeUUID) {
+    var result=[];
+    var exists=await existsFile(storage + 'private','mySharedRoutes.ttl');
+    if(exists)
+    {
+        const mySharedRoutesDocument = await fetchDocument(storage + 'private/mySharedRoutes.ttl');
+        
+        let rutas = mySharedRoutesDocument.getAllSubjectsOfType('http://arquisoft.github.io/viadeSpec/route');
+        for (var e = 0; e < rutas.length; e++) {
+          //Miro a ver si estoy compartiendo esta ruta
+          if(rutas[e].getLiteral(schema.identifier)===routeUUID)
+          {
+              //Si la estoy compartiendo entonces saco los amigos con los que la comparto
+              let amigos=rutas[e].getAllRefs(schema.agent)
+              for (var i = 0; i < amigos.length; i++) {
+                  //los añado a result
+                  result = [...result, amigos];
+              }
+              //Ya encontre lo que busco asi que salgo
+              break;
+          }
+        }
+    }
+    return result;
+}
+
 async function sendCommentNotification(webId,friendWebId,routeUUID) {
     return sendNotificationBody(webId,friendWebId,
     `@prefix as: <https://www.w3.org/ns/activitystreams#> .
