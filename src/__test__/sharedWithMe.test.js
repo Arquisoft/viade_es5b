27e6b3c910deca97550dbf00e3c73d517cd2b255
DiegoMarcosPerez <47299591+DiegoMarcosPerez@users.noreply.github.com>
2020-04-01 19:00:49 +0200

    SharedWithMe ya renderiza

--- src/__test__/sharedWithMe.test.js
+++ src/__test__/sharedWithMe.test.js
@@ -1,18 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import RouteCard from "../front-end/components/ruta/VerRutas/RouteCard";
-import Ruta from "../front-end/model/Ruta";
-import Hito from "../front-end/model/Hito";
-import { render, cleanup, fireEvent, screen } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import SharedWithMe from "../front-end/components/share/SharedWithMe";
 import RutaService from "../__test__/__mocks__/RutaService";
 
 let rutaService = new RutaService();
 
-test("Se renderizan bien los componentes de SharePanel", () => {
+test("Se renderizan bien los componentes de SharedWithMe", () => {
     afterAll(cleanup);
-    const { getByTestId } = render(<SharedWithMe getRutasCompartidasConmigo={rutaService.getRutasCompartidasConmigo()}
+    const { getByTestId } = render(<SharedWithMe 
+      handleLoaded={rutaService.handleLoaded}
+      getRutasCompartidasConmigo={rutaService.getRutasCompartidasConmigo}
       subirFicheroAMiRuta={rutaService.subirFicheroAMiRuta}
       obtenerFicherosRuta={rutaService.obtenerFicherosRuta}
       comentarMiRuta={rutaService.comentarMiRuta}
