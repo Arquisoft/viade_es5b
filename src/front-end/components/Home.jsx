fe73466c740ebb8c5bd54d384cc13fc655a8b415
Alex Florez <UO264371@uniovi.es>
2020-04-01 20:53:22 +0200

    Dejando la vista de Home algo más presentable

--- src/front-end/components/Home.jsx
+++ src/front-end/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import HomeLogin from "./authentication/login/HomeLogin";
 /**
  * Componente que representa la página principal de la
  * aplicación.
@@ -7,7 +8,8 @@ class Home extends Component {
   render() {
     return (
       <div className="jumbotron">
-        <h1 className="display-3 text-center">Bienvenido a Viade!</h1>
+        <h1 className="display-3">¡Bienvenido a Viade!</h1>
+        <HomeLogin />
       </div>
     );
   }
